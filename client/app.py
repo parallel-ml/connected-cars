92d79d3f2bbbf2b778fd31202f39a566387f97ab
Nima Shoghi <nimashoghi@gmail.com>
2019-10-03 18:04:58 +0000

    Updated servers to use websockets instead of REST api

--- client/app.py
+++ client/app.py
@@ -1,83 +1,91 @@
+import asyncio
 import base64
 import multiprocessing as mp
 import os
+import struct
+from concurrent.futures.process import ProcessPoolExecutor
 
 import requests
+import websockets
 from rplidar import RPLidar as Lidar
 
 from breezyslam.algorithms import RMHC_SLAM
 from breezyslam.sensors import RPLidarA1 as LaserModel
 
-MAP_SIZE_PIXELS = int(os.environ.get("SLAM_MAP_SIZE_PIXELS", "500"))
-MAP_SIZE_METERS = int(os.environ.get("SLAM_MAP_SIZE_METERS", "10"))
 LIDAR_DEVICE = os.environ.get("SLAM_LIDAR_DEVICE", "/dev/ttyUSB0")
 MIN_SAMPLES = int(os.environ.get("SLAM_MIN_SAMPLES", "200"))
-SERVER_URL = os.environ.get("SLAM_SERVER_URL", "http://localhost:8080")
+SERVER_URL = os.environ.get("SLAM_SERVER_URL", "ws://localhost:80")
 
+MAP_SIZE_PIXELS = int(os.environ.get("SLAM_MAP_SIZE_PIXELS", "500"))
+MAP_SIZE_METERS = int(os.environ.get("SLAM_MAP_SIZE_METERS", "10"))
+MAP_BYTES_SIZE = int(MAP_SIZE_PIXELS * MAP_SIZE_PIXELS)
+STRUCT_FORMAT = f"<fff{MAP_BYTES_SIZE}s"
 
-def send_info_target(x, y, theta, mapbytes):
-    requests.post(
-        f"{SERVER_URL}/submit_data",
-        data={"x": x, "y": y, "theta": theta, "mapbytes": base64.encodebytes(mapbytes)},
-    )
+async def send_info(x, y, theta, mapbytes):
+    async with websockets.connect(SERVER_URL) as websocket:
+        packed = struct.pack(STRUCT_FORMAT, x, y, theta, mapbytes)
+        websocket.send(packed)
 
-
-def send_info(x, y, theta, mapbytes):
-    mp.Process(target=send_info_target, args=(x, y, theta, mapbytes)).start()
-
+def send_info_process(x, y, theta, mapbytes):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(send_info(x, y, theta, mapbytes))
 
 def main():
-    # Connect to Lidar unit
-    lidar = Lidar(LIDAR_DEVICE)
+    with ProcessPoolExecutor(5) as executor:
+        event_loop = asyncio.get_event_loop()
 
-    # Create an RMHC SLAM object with a laser model and optional robot model
-    slam = RMHC_SLAM(LaserModel(), MAP_SIZE_PIXELS, MAP_SIZE_METERS)
+        # Connect to Lidar unit
+        lidar = Lidar(LIDAR_DEVICE)
 
-    # Initialize an empty trajectory
-    trajectory = []
+        # Create an RMHC SLAM object with a laser model and optional robot model
+        slam = RMHC_SLAM(LaserModel(), MAP_SIZE_PIXELS, MAP_SIZE_METERS)
 
-    # Initialize empty map
-    mapbytes = bytearray(MAP_SIZE_PIXELS * MAP_SIZE_PIXELS)
+        # Initialize an empty trajectory
+        trajectory = []
 
-    # Create an iterator to collect scan data from the RPLidar
-    iterator = lidar.iter_scans()
+        # Initialize empty map
+        mapbytes = bytearray(MAP_SIZE_PIXELS * MAP_SIZE_PIXELS)
 
-    # We will use these to store previous scan in case current scan is inadequate
-    previous_distances = None
-    previous_angles = None
+        # Create an iterator to collect scan data from the RPLidar
+        iterator = lidar.iter_scans()
 
-    # First scan is crap, so ignore it
-    next(iterator)
+        # We will use these to store previous scan in case current scan is inadequate
+        previous_distances = None
+        previous_angles = None
 
-    while True:
-        # Extract (quality, angle, distance) triples from current scan
-        items = [item for item in next(iterator)]
+        # First scan is crap, so ignore it
+        next(iterator)
 
-        # Extract distances and angles from triples
-        distances = [item[2] for item in items]
-        angles = [item[1] for item in items]
+        while True:
+            # Extract (quality, angle, distance) triples from current scan
+            items = [item for item in next(iterator)]
 
-        # Update SLAM with current Lidar scan and scan angles if adequate
-        if len(distances) > MIN_SAMPLES:
-            slam.update(distances, scan_angles_degrees=angles)
-            previous_distances = distances.copy()
-            previous_angles = angles.copy()
+            # Extract distances and angles from triples
+            distances = [item[2] for item in items]
+            angles = [item[1] for item in items]
 
-        # If not adequate, use previous
-        elif previous_distances is not None:
-            slam.update(previous_distances, scan_angles_degrees=previous_angles)
+            # Update SLAM with current Lidar scan and scan angles if adequate
+            if len(distances) > MIN_SAMPLES:
+                slam.update(distances, scan_angles_degrees=angles)
+                previous_distances = distances.copy()
+                previous_angles = angles.copy()
 
-        # Get current robot position
-        x, y, theta = slam.getpos()
+            # If not adequate, use previous
+            elif previous_distances is not None:
+                slam.update(previous_distances, scan_angles_degrees=previous_angles)
 
-        # Get current map bytes as grayscale
-        slam.getmap(mapbytes)
+            # Get current robot position
+            x, y, theta = slam.getpos()
 
-        send_info(x / 1000.0, y / 1000.0, theta, mapbytes)
+            # Get current map bytes as grayscale
+            slam.getmap(mapbytes)
 
-    # Shut down the lidar connection
-    lidar.stop()
-    lidar.disconnect()
+            mp.Process(target=send_info_process,args=(x,y,theta,mapbytes)).start()
+
+        # Shut down the lidar connection
+        lidar.stop()
+        lidar.disconnect()
 
 
 if __name__ == "__main__":
--- server/app.py
+++ server/app.py
@@ -1,26 +1,31 @@
+import asyncio
 import base64
 import os
+import struct
 
-from flask import Flask, request
+import websockets
 
 from roboviz import MapVisualizer
 
 MAP_SIZE_PIXELS = int(os.environ.get("SLAM_MAP_SIZE_PIXELS", "500"))
 MAP_SIZE_METERS = int(os.environ.get("SLAM_MAP_SIZE_METERS", "10"))
-
-app = Flask(__name__)
+MAP_BYTES_SIZE = int(MAP_SIZE_PIXELS * MAP_SIZE_PIXELS)
+STRUCT_FORMAT = f"<fff{MAP_BYTES_SIZE}s"
 
 # Set up a SLAM display
 viz = MapVisualizer(MAP_SIZE_PIXELS, MAP_SIZE_METERS, "SLAM")
 
+async def data_handler(websocket, path):
+    data = await websocket.recv()
+    while data:
+        x, y, theta, mapbytes_str = struct.unpack(STRUCT_FORMAT, data)
+        mapbytes = bytearray(mapbytes_str)
 
-@app.route("/submit_data", methods=["POST"])
-def submit_data():
-    print(request.form["x"], request.form["y"], request.form["theta"])
-    if not viz.display(
-        float(request.form["x"]),
-        float(request.form["y"]),
-        float(request.form["theta"]),
-        bytearray(base64.b64decode(request.form["mapbytes"])),
-    ):
-        exit(0)
+        if not viz.display(x, y, theta, mapbytes):
+            exit(0)
+        data = await websocket.recv()
+
+
+if __name__ == "__main__":
+    start_server = websockets.serve(data_handler, host="0.0.0.0")
+    asyncio.run(start_server)
